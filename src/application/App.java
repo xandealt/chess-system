4b106c711fd626c8ca86310cbb067398863e39bb
Alexandre Augusto <alexandre_alt@outlook.com>
2023-06-06 14:15:46 -0300

    First class: Position

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
