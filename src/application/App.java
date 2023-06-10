1b478d5e38b2e2844fafe137e438164686690474
Alexandre Augusto <alexandre_alt@outlook.com>
2023-06-10 12:32:59 -0300

    Checkmate logic

--- src/application/App.java
+++ src/application/App.java
@@ -16,7 +16,7 @@ public class App {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -45,5 +45,7 @@ public class App {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
